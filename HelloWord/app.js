19075da50e1ca8c71a941efc97455e9759cbd0cc
niaidaye <645992178@qq.com>
2017-11-02 19:54:21 +0800

    对node.js的学习心得

---
+++ HelloWord/app.js
@@ -0,0 +1,10 @@
+/**
+ * 2017/11/2
+ * 作者: 64599
+ * 功能: 万年不变的hello word
+ */
+
+console.log("helloword");
+
+console.log("hello nodejs");
+//会调用，c/c++写的中间件
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Nodejs
 这是一个关于nodejs的学习之路
+## 万年不变的hello word
+>console.log("helloword");
+ 
+>console.log("hello nodejs");
+
+## Nodej.JS和 Javascript的关系
