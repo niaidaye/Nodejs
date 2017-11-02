5c24b6eda2539c2d79652552707036082e750c30
niaidaye <645992178@qq.com>
2017-11-02 20:28:30 +0800

    搭建简易的Http服务器

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 >console.log("hello nodejs");
 
 ## Nodej.JS和 Javascript的关系
+
+## 搭建简易的Http服务器
+创建一个简易的HttpServer
+  * 为什么Node.JS搭建Server这么简单，因为工具方法强大，本身Node.JS也是一个容器
---
+++ 搭建简易的Http服务器/app.js
@@ -0,0 +1,39 @@
+/**
+ * 2017/11/2
+ * 作者: 64599
+ * 功能: 搭建简易的HttpServer
+ */
+
+var httpServer = require('http');//导入HTTP模块
+var server = httpServer.createServer(function (request, response) {
+
+    response.writeHead(200,{"Content-Type" : "text/plain"});
+    response.write("Hello Buddy ");
+    response.end("ok");
+
+    console.log("Request Response OK");
+
+});
+
+server.listen(3000);
+console.log("Server Started...");
+
+
+
+
+//例子
+
+// var httpServer = require( 'http' ) // 导入Node.JS库中的Http的库文件，并获取句柄
+//
+// var server = httpServer.createServer(function ( request, response ) {
+//
+//     console.log( "Get An Client Request")
+//     response.writeHead( 200, { "Content-Type" : "text/plain"} );
+//     response.write( "Hello Buddy ")
+//     response.end( )
+//
+//     console.log( "Request  Response OK")
+// })
+//
+//
+// server.listen( 3000 )
