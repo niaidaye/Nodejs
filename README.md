0ee8fa05f2ac64ac06b00dbd0d8ee722003f5921
niaidaye <645992178@qq.com>
2017-11-02 20:59:08 +0800

    分析简易的服务器的各个模块

--- README.md
+++ README.md
@@ -10,3 +10,7 @@
 ## 搭建简易的Http服务器
 创建一个简易的HttpServer
   * 为什么Node.JS搭建Server这么简单，因为工具方法强大，本身Node.JS也是一个容器
+
+## 分析简易的服务器的各个模块
+Node.JS Http Server 请求和响应的原理
+  1. 我们创建一个Server对象（利用createServer 和 回掉函数），当我们的Server获取的到了请求的时候，我们Node.JS就会通过Node.JS中的事件分发机制调用我们的回掉函数，从而我们能够获取到请求的信息
---
+++ 分析简易的服务器的各个模块/app.js
@@ -0,0 +1,13 @@
+/**
+ * 2017/11/2
+ * 作者: 64599
+ * 功能:
+ */
+var http = require ("http");
+var server = http.createServer( function (require, response) {
+
+    response.writeHead( 200, {"Content-type" : "text-plain"});
+    response.write("ok");
+    response.end();
+});
+server.listen( 3000 );
